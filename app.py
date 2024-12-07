9d9489a30e942d9b1ba089c5d10581f6f45d9edc
Dhanush0660 <88339584+Dhanush0660@users.noreply.github.com>
2024-12-08 02:41:08 +0530

    Update app.py
    bug fixes

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def ask_json():
 
 
 def start_app():
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=False)
 
 if __name__ == "__main__":
     start_app()
